7ee8e1895fcd38c950595acd074a4b3b4389f6ac
BentleyCreated <wazap7890@gmail.com>
2023-11-21 19:33:04 -0500

    finished run stats graph

--- ui/main_menu.py
+++ ui/main_menu.py
@@ -25,26 +25,38 @@ from .searching_menu import Searching_Menu
 
 class Main_Menu(QMainWindow):
     def __init__(self, ui_context: UI_Context):
-        # initialize the main menu's window
         super().__init__()
         self.setWindowTitle("Tandapay Simulation")
-        self.resize(300, 360)
-
-        # create ui element factory for creating UI elements
+        self.resize(300, 350)
         self.uief = UI_Element_Factory()
-        
-        # will store all the callbacks for buttons in the main menu
         self.uic = ui_context
 
-        # set up layout for the main menu
+        # Set the global style sheet
+        self.setStyleSheet("""
+            QMainWindow {
+                background-color:  #2b2b2b; 
+                color: #FFFFFF;
+            }
+            QPushButton {
+                background-color: #555555;
+                border: 3px solid #FFFFFF;
+                padding: 5px;
+                color: white;
+            }
+            QPushButton:hover {
+                background-color: #666666;
+            }
+            QLabel {
+                font-size: 35px;
+            }
+        """)
+
         self.layout = QVBoxLayout()
-        
-        # create the title at the top of the menu
         self.title = self.get_title_widget()
         self.layout.addWidget(self.title)
         self.layout.addStretch(1)
-        
-        # make buttons for main menu
+
+        # Create buttons with the new style
         self.run_simulation_btn = self.uief.make_push_button_element("Run Simulation", None, self.run_simulation)
         self.layout.addWidget(self.run_simulation_btn)
 
@@ -69,8 +81,6 @@ class Main_Menu(QMainWindow):
         self.quit_btn = self.uief.make_push_button_element("Quit", None, self.quit)
         self.layout.addWidget(self.quit_btn)
 
-        # finally, set this as the central widget. The central widget
-        # is simply the QT widget that occupies all the space in the main window
         self.central_widget = QWidget()
         self.central_widget.setLayout(self.layout)
         self.setCentralWidget(self.central_widget)
@@ -90,23 +100,23 @@ class Main_Menu(QMainWindow):
         
 
 
-       # mean = statistics_aggregator.mean_dict[self.uic.ov_obj.selected_graph]
-       # std_dev = statistics_aggregator.std_dict[self.uic.ov_obj.selected_graph]
+        mean = statistics_aggregator.mean_dict[self.uic.ov_obj.selected_graph]
+        std_dev = statistics_aggregator.std_dict[self.uic.ov_obj.selected_graph]
 
         # Creating a range around the mean, covering 99.7% of the data (3 standard deviations)
-       # x = np.linspace(mean - 3*std_dev, mean + 3*std_dev, 1000)
-       # y = stats.norm.pdf(x, mean, std_dev)
+        x = np.linspace(mean - 3*std_dev, mean + 3*std_dev, 1000)
+        y = stats.norm.pdf(x, mean, std_dev)
 
         # Plotting the bell curve
-       # plt.plot(x, y)
+        plt.plot(x, y)
 
         # Adding titles and labels
-       # plt.title(f'Normal Distribution of {self.uic.ov_obj.selected_graph}', fontsize=14, fontweight='bold')
-       # plt.xlabel(f'{self.uic.ov_obj.selected_graph}', fontsize=12, fontweight='bold')
-       # plt.ylabel('Probability Density', fontsize=12, fontweight='bold')
+        plt.title(f'Normal Distribution of {self.uic.ov_obj.selected_graph}', fontsize=14, fontweight='bold')
+        plt.xlabel(f'{self.uic.ov_obj.selected_graph}', fontsize=12, fontweight='bold')
+        plt.ylabel('Probability Density', fontsize=12, fontweight='bold')
 
         # Showing the plot
-       # plt.show()
+        plt.show()
 
 
 
@@ -136,8 +146,9 @@ class Main_Menu(QMainWindow):
         title = QLabel("Tandapay")
         title_font = QFont()
         title_font.setBold(True)
-        title_font.setPointSize(18)
+        title_font.setPointSize(64)  # Increase the font size
         title.setFont(title_font)
         title.setAlignment(Qt.AlignCenter)
+        title.setStyleSheet("color: white;")  # Set the font color to white
         return title
 
